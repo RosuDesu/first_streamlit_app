e3540cec7cb3aefd4d486562d02eef2f0ebc9fac
RosuDesu <117959379+RosuDesu@users.noreply.github.com>
2022-11-11 23:41:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,20 @@ my_cur.execute("insert into fruit_load_list values('from streamlit')")
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#create the repoeatable code block ( called function)
+def get_fruityvice_date(this_fruit_choice):
+    fruityvice_response = request.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return  fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
     else:
-        fruityvice_response = request.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruity_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
                                         
 except URLError as e:
    streamlit.error()
