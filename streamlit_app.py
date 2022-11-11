332540db40e79e99512014637407120664c83b43
RosuDesu <117959379+RosuDesu@users.noreply.github.com>
2022-11-11 10:36:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.test('Omega3 and blueberry oatmeal\nkale, spinach, and roclet smoothie\nHard-Boiled Free-Range egg')
+streamlit.test('Omega3 and Blueberry Oatmeal)
+streamlit.text('Kale, Spinach, and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range egg')
