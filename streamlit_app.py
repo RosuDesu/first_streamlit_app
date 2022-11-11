9813fa68ee82ff03749d1c9f6071af9dbb0b8b65
RosuDesu <117959379+RosuDesu@users.noreply.github.com>
2022-11-11 23:37:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,21 +26,6 @@ my_cur.execute("insert into fruit_load_list values('from streamlit')")
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
-
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
-streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
