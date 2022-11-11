ecf307a9e7ca43d3f0c86ba9e52bb0f9ff8ac597
RosuDesu <117959379+RosuDesu@users.noreply.github.com>
2022-11-11 10:41:18 +0800

    Update streamlit_app.py
    Correction on some missing '

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range egg')
-streamlit.text('🥑🍞 Avocado Toast)
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
