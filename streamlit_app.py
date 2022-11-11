c06b197b7ce905361b3ed101489ecc0692f904fc
RosuDesu <117959379+RosuDesu@users.noreply.github.com>
2022-11-12 00:05:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
@@ -41,7 +40,6 @@ try:
         streamlit.dataframe(back_from_function)                        
 except URLError as e:
    streamlit.error()
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
 streamlit.header('View Our Fuit List - Add your Favorites!:')
